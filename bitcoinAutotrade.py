85fb683f493d3fd12524a5656772764b5c5473cf
poerf12 <50770561+poerf12@users.noreply.github.com>
2021-06-30 07:50:53 +0900

    Update bitcoinAutotrade.py

--- bitcoinAutotrade.py
+++ bitcoinAutotrade.py
@@ -6,19 +6,19 @@ access = "nMdEtad6mi5tU8vqkfhfE9FXR8TjvqsX6SIPiB1E"
 secret = "DoDe7Bz5AsH8X6ifdpy6F0CImLMzTDsYTonZaY6i"
 
 def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
+    """price"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
+    """time"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_balance(ticker):
-    """잔고 조회"""
+    """balance"""
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -29,14 +29,14 @@ def get_balance(ticker):
     return 0
 
 def get_current_price(ticker):
-    """현재가 조회"""
+    """current price"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
-# 로그인
+# login
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-# 자동매매 시작
+# start trading
 while True:
     try:
         now = datetime.datetime.now()
